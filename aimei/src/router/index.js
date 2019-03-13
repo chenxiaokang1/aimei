d4b180fc9cd0e0cf6420334b35a43e4f1a93fa9c
lileilei <784957430@qq.com>
2019-03-13 18:40:01 +0800

    111

--- aimei/src/router/index.js
+++ aimei/src/router/index.js
@@ -18,77 +18,177 @@ import Detail from '@/components/me/Detail'
 import Zsxm from '@/components/me/Zsxm'
 import Gou from '@/components/me/Gou'
 
+
+
+// 李雷雷
+import myCenter from '@/components/my_center/center_index'
+import reward from '@/components/my_center/reward'
+import love from '@/components/my_center/love'
+import rank from '@/components/my_center/ranking'
+import rewardCenter from '@/components/my_center/reward_center'
+import myOrder from '@/components/my_center/my_order'
+import addressAdd from '@/components/my_center/address_add'
+import address from '@/components/my_center/address'
+import hb from '@/components/my_center/hb'
+import teamIndex from '@/components/my_center/my_team'
+import bigData from '@/components/my_center/big_data'
+import myFans from '@/components/my_center/my_fans'
+import myTeam from '@/components/my_center/team_kinds'
+import brand from '@/components/my_center/brand'
+import problem from '@/components/my_center/problem'
+import setinfo from '@/components/my_center/set_info'
+import bindPhone from '@/components/my_center/bind_phone'
+import giveLove from '@/components/my_center/give_love'
+import loveOrder from '@/components/my_center/love_order'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'HelloWorld',
-      redirect:"/home",
+      redirect: "/home",
       component: HelloWorld,
-      children:[
-      	{
-      		path: '/home',
-      		component: Home
-      	},
-      	{
-					path: '/zheng',
-					redirect:"/zheng/quan",
-					component: Zheng,
-					children:[
-						{
-							path: '/zheng/quan',
-							component: Quan
-						},
-						{
-							path: '/zx',
-							component: Zx
-						},
-						{
-							path: '/sx',
-							component: Sx
-						},
-						{
-							path: '/mr',
-							component:Mr
-						},
-						{
-							path: '/xz',
-							component:Xz
-						},
-						{
-							path: '/ns',
-							component:Ns
-						},
-					]
-      	},
-      	{
-      		path: '/super',
-      		component: Super
-		  },
-		  {
-			path: '/ys',
-			component: Ys
-		},
-		
+      children: [{
+          path: '/home',
+          component: Home
+        },
+        {
+          path: '/zheng',
+          redirect: "/zheng/quan",
+          component: Zheng,
+          children: [{
+              path: '/zheng/quan',
+              component: Quan
+            },
+            {
+              path: '/zx',
+              component: Zx
+            },
+            {
+              path: '/sx',
+              component: Sx
+            },
+            {
+              path: '/mr',
+              component: Mr
+            },
+            {
+              path: '/xz',
+              component: Xz
+            },
+            {
+              path: '/ns',
+              component: Ns
+            },
+          ]
+        },
+        {
+          path: '/super',
+          component: Super
+        }, ,
+        {
+          path: '/myCenter',
+          component: myCenter
+        },
+        {
+          path: '/ys',
+          component: Ys
+        },
+
       ]
-	},
-	{
-		path: '/bj',
-		component: Bj
-	},
-	{
-		path: '/zsxm',
-		component: Zsxm
-	},
-	{
-		path: '/gou',
-		component: Gou
-	},
+    },
     {
-			path: '/detail',
-			component: Detail
+      path: '/bj',
+      component: Bj
+    },
+    {
+      path: '/zsxm',
+      component: Zsxm
+    },
+    {
+      path: '/gou',
+      component: Gou
+    },
+    {
+      path: '/detail',
+      component: Detail
+	},
+	
+	// lilielei
+	,
+    {
+      path: '/reward',
+      component: reward
+    },
+    {
+      path: '/love',
+      component: love
 		},
+		{
+      path: '/rank',
+      component: rank
+		},
+		{
+      path: '/rewardCenter',
+      component: rewardCenter
+    },
+		{
+      path: '/myOrder',
+      component: myOrder
+    },
+    {
+      path: '/addressAdd',
+      component: addressAdd
+    },
+    {
+      path: '/address',
+      component: address
+    },
+    {
+      path: '/hb',
+      component: hb
+    },
+    {
+      path: '/teamIndex',
+      component: teamIndex
+    },
+    {
+      path: '/bigData',
+      component: bigData
+    },
+    {
+      path: '/myFans',
+      component: myFans
+    },
+    {
+      path: '/myTeam',
+      component: myTeam
+    },
+    {
+      path: '/brand',
+      component: brand
+    },
+    {
+      path: '/problem',
+      component: problem
+    },
+    {
+      path: '/setinfo',
+      component: setinfo
+    },
+    {
+      path: '/bindPhone',
+      component: bindPhone
+    },
+    {
+      path: '/giveLove',
+      component: giveLove
+    }
+    ,
+    {
+      path: '/loveOrder',
+      component: loveOrder
+    }
+    
   ]
 })
